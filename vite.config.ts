e8231e4a7ac8ffd8af4da0f9d429ebae51a54c28
Zapsteer <134844372+Zapsteer@users.noreply.github.com>
2026-02-27 12:56:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { inspectAttr } from 'kimi-plugin-inspect-react'
-
 // https://vite.dev/config/
+// force rebuild v3
 export default defineConfig({
   base: './',
   plugins: [inspectAttr(), react()],
